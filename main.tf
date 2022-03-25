32654b15f655737678bdeddec722a8537060d1e6
Dennis Zelada <denniszelada@gmail.com>
2022-03-25 21:21:21 +0100

    Add ec2 t2.micro with ubuntu

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ provider "aws" {
   region = "eu-central-1"
   profile = "winback"
 }
+
+resource "aws_instance" "example" {
+  ami       = "ami-0dcc0ebde7b2e00db"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "terraform-example"
+  }
+}
