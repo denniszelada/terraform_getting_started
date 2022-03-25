9b3ecac5db527699dafd966a0322ab939b5c14ad
Dennis Zelada <denniszelada@gmail.com>
2022-03-25 21:30:15 +0100

    Add a small server with busybox that will reply Hello World on ubuntu

--- main.tf
+++ main.tf
@@ -6,7 +6,13 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami       = "ami-0dcc0ebde7b2e00db"
   instance_type = "t2.micro"
-  
+
+  user_data = <<-EOF
+                 #!/bin/bash
+                 echo "Hello, World" > index.html
+                 nohup busybox httpd -f -p 8080 &
+                 EOF
+
   tags = {
     Name = "terraform-example"
   }
