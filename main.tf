baab93a75df2ff412c099255da08bd0d9c051365
Dennis Zelada <denniszelada@gmail.com>
2022-03-29 10:18:11 +0200

    add launch configuration for autoscaling group

--- main.tf
+++ main.tf
@@ -9,19 +9,32 @@ variable "server_port" {
   default     = 8080
 }
 
-resource "aws_instance" "example" {
-  ami       = "ami-0dcc0ebde7b2e00db"
+resource "aws_launch_configuration" "example" {
+  image_id       = "ami-0dcc0ebde7b2e00db"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
                  #!/bin/bash
                  echo "Hello, World" > index.html
                  nohup busybox httpd -f -p ${var.server_port} &
                  EOF
+  lifecycle {
+    create_before_destroy = true
+  }
 
-  tags = {
-    Name = "terraform-example"
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
