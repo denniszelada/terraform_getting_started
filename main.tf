2a173f75c679240f99986616c6776ca99ac8ee0f
Dennis Zelada <denniszelada@gmail.com>
2022-03-29 10:20:17 +0200

    Add data source to read the default VPC, and add the information to the zone identifier on the autoscaling group

--- main.tf
+++ main.tf
@@ -25,8 +25,13 @@ resource "aws_launch_configuration" "example" {
 
 }
 
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
 
   min_size = 2
   max_size = 10
